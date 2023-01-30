03794e9e07b98def4db55446ff62be7c203f25d6
arman <arman@DESKTOP-2T4I1CC>
2023-01-30 17:38:42 -0300

    Adicionar arquivos de projeto.

---
+++ 01-ByteBank/ContaCorrente.cs
@@ -0,0 +1,34 @@
+﻿public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+
+    public bool Sacar(double valor)
+    {
+        if (this.saldo < valor) //O this serve pra pegar o saldo da instancia que o método for usado, e não do valor padrão da classe
+        {
+            return false;
+        }
+        else
+            this.saldo -= valor;
+            return true;
+    }
+
+    public void Depositar(double valor)
+    {
+        this.saldo += valor;
+    }
+
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        this.saldo -= valor;
+        contaDestino.Depositar(valor);
+        return true;
+    }
+}
---
+++ 01-ByteBank/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_ByteBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Testando os métodos
+            
+            ContaCorrente contaTeste = new ContaCorrente();
+
+            //Tentando sacar um valor maior que o valor default da conta
+
+            bool ResultadoDoSaque = contaTeste.Sacar(200); //Retornará false, pois 200 é maior do que o valor que tem na conta
+            if (ResultadoDoSaque)
+            {
+                Console.WriteLine("Saque realizado com sucesso!");
+            }
+            else
+                Console.WriteLine("Não foi possível realizar o saque!");
+
+            Console.WriteLine(ResultadoDoSaque);
+
+            //Agora veja o atual valor que tem na conta:
+            Console.WriteLine("Saldo antes do depósito.");
+            Console.WriteLine(contaTeste.saldo);
+            //E agora veja o valor que vai ficar depois do depósito
+            contaTeste.Depositar(200); //Certamente retornará 300
+            Console.WriteLine("Saldo depois do depósito.");
+            Console.WriteLine(contaTeste.saldo);
+
+            //Agora vamos criar uma conta para fazer a transferência
+            ContaCorrente contaTransferencia = new ContaCorrente();
+
+            contaTeste.Transferir(100, contaTransferencia); //Após essa transferência, certamente teremos o valor 200 na conta teste, pois somamos os 100 com
+            //os 200 depositados e tinhamos 300, mas depois de transferir 100 ficamos com 200, e a contaTransferencia também terá 200, pois além dos 100 default,
+            //ela recebeu mais 100 da contaTeste
+            Console.WriteLine("TRANSFERÊNCIA REALIZADA!");
+
+            Console.WriteLine("Saldo da conta teste depois da transferência.");
+            Console.WriteLine(contaTeste.saldo);
+            Console.WriteLine("Saldo da conta Transferência depois da TRANSFERÊNCIA.");
+            Console.WriteLine(contaTransferencia.saldo);
+
+            Console.ReadLine(); //Não apaga o readline, se apagar o console nem abre direito.
+        }
+    }
+}
